b32c2d15a0865be398397da33cb32fe340388d5a
Juan Ramírez Ruiz <juan.ramirez.ruiz@gmail.com>
2017-06-06 19:40:37 +0200

    Refactored tests.

--- test/app.js
+++ test/app.js
@@ -3,51 +3,41 @@ let expect = chai.expect
 let app = require('src/app')
 
 describe('app', () => {
-  it('will return the input as a string if it is a 1', () => {
-    expect(app.translate(1)).to.be.eq('1')
-  })
+  describe('.translate', () => {
+    it('will return the input as a string if it is a 1', () =>
+      expect(app.translate(1)).to.be.eq('1'))
 
-  it('will return the input as a string if it is a 2', () => {
-    expect(app.translate(2)).to.be.eq('2')
-  })
+    it('will return the input as a string if it is a 2', () =>
+      expect(app.translate(2)).to.be.eq('2'))
 
-  it('will return the input as a string if it is a 4', () => {
-    expect(app.translate(4)).to.be.eq('4')
-  })
+    it('will return the input as a string if it is a 4', () =>
+      expect(app.translate(4)).to.be.eq('4'))
 
-  it('will return the input as a string if it is a 7', () => {
-    expect(app.translate(7)).to.be.eq('7')
-  })
+    it('will return the input as a string if it is a 7', () =>
+      expect(app.translate(7)).to.be.eq('7'))
 
-  it('will return Fizz if the input is 3', () => {
-    expect(app.translate(3)).to.be.eq('Fizz')
-  })
+    it('will return Fizz if the input is 3', () =>
+      expect(app.translate(3)).to.be.eq('Fizz'))
 
-  it('will return Fizz if the input is 6', () => {
-    expect(app.translate(6)).to.be.eq('Fizz')
-  })
+    it('will return Fizz if the input is 6', () =>
+      expect(app.translate(6)).to.be.eq('Fizz'))
 
-  it('will return Fizz if the input is 9', () => {
-    expect(app.translate(9)).to.be.eq('Fizz')
-  })
+    it('will return Fizz if the input is 9', () =>
+      expect(app.translate(9)).to.be.eq('Fizz'))
 
-  it('will return Buzz if the input is 5', () => {
-    expect(app.translate(5)).to.be.eq('Buzz')
-  })
+    it('will return Buzz if the input is 5', () =>
+      expect(app.translate(5)).to.be.eq('Buzz'))
 
-  it('will return Buzz if the input is 10', () => {
-    expect(app.translate(10)).to.be.eq('Buzz')
-  })
+    it('will return Buzz if the input is 10', () =>
+      expect(app.translate(10)).to.be.eq('Buzz'))
 
-  it('will return Buzz if the input is 20', () => {
-    expect(app.translate(20)).to.be.eq('Buzz')
-  })
+    it('will return Buzz if the input is 20', () =>
+      expect(app.translate(20)).to.be.eq('Buzz'))
 
-  it('will return FizzBuzz if the input is 15', () => {
-    expect(app.translate(15)).to.be.eq('FizzBuzz')
-  })
+    it('will return FizzBuzz if the input is 15', () =>
+      expect(app.translate(15)).to.be.eq('FizzBuzz'))
 
-  it('will return FizzBuzz if the input is 30', () => {
-    expect(app.translate(30)).to.be.eq('FizzBuzz')
+    it('will return FizzBuzz if the input is 30', () =>
+      expect(app.translate(30)).to.be.eq('FizzBuzz'))
   })
 })
