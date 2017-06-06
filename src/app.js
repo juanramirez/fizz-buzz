478afc581d922e76ec4ffba8193a7c3374f1d224
Juan Ramírez Ruiz <juan.ramirez.ruiz@gmail.com>
2017-06-06 19:17:22 +0200

    translate() refactored.

--- src/app.js
+++ src/app.js
@@ -1,11 +1,5 @@
 module.exports = {
   translate: (input) => {
-    if (input === 1)
-      return 1
-    if (input === 2)
-      return 2
-    if (input === 4)
-      return 4
-    return 7
+    return input
   }
 }
