ac137cc7db4269b6ee8018c0cfd53c00c21ae10d
Juan Ramírez Ruiz <juan.ramirez.ruiz@gmail.com>
2017-06-06 19:36:20 +0200

    Refactored translate() for multiples of 15.

--- src/app.js
+++ src/app.js
@@ -1,8 +1,6 @@
 module.exports = {
   translate: (input) => {
-    if (input === 15)
-      return 'FizzBuzz'
-    if (input === 30)
+    if (input % 15 === 0)
       return 'FizzBuzz'
     if (input % 5 === 0)
       return 'Buzz'
