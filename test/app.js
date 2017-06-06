9f1cb6f718d60711190890c244ce623cacfd16f1
Juan Ramírez Ruiz <juan.ramirez.ruiz@gmail.com>
2017-06-06 20:34:24 +0200

    Refactored tests.

--- test/app.js
+++ test/app.js
@@ -8,39 +8,43 @@ describe('app', () => {
       expect(app.getSequence().length).to.be.eq(100))
 
     it('will return the input as a string if it is a 1', () =>
-      expect(app.getSequence()[1 - 1]).to.be.eq('1'))
+      expect(getSequencePosition(1)).to.be.eq('1'))
 
     it('will return the input as a string if it is a 2', () =>
-      expect(app.getSequence()[2 - 1]).to.be.eq('2'))
+      expect(getSequencePosition(2)).to.be.eq('2'))
 
     it('will return the input as a string if it is a 4', () =>
-      expect(app.getSequence()[4 - 1]).to.be.eq('4'))
+      expect(getSequencePosition(4)).to.be.eq('4'))
 
     it('will return the input as a string if it is a 7', () =>
-      expect(app.getSequence()[7 - 1]).to.be.eq('7'))
+      expect(getSequencePosition(7)).to.be.eq('7'))
 
     it('will return Fizz if the input is 3', () =>
-      expect(app.getSequence()[3 - 1]).to.be.eq('Fizz'))
+      expect(getSequencePosition(3)).to.be.eq('Fizz'))
 
     it('will return Fizz if the input is 6', () =>
-      expect(app.getSequence()[6 - 1]).to.be.eq('Fizz'))
+      expect(getSequencePosition(6)).to.be.eq('Fizz'))
 
     it('will return Fizz if the input is 9', () =>
-      expect(app.getSequence()[9 - 1]).to.be.eq('Fizz'))
+      expect(getSequencePosition(9)).to.be.eq('Fizz'))
 
     it('will return Buzz if the input is 5', () =>
-      expect(app.getSequence()[5 - 1]).to.be.eq('Buzz'))
+      expect(getSequencePosition(5)).to.be.eq('Buzz'))
 
     it('will return Buzz if the input is 10', () =>
-      expect(app.getSequence()[10 - 1]).to.be.eq('Buzz'))
+      expect(getSequencePosition(10)).to.be.eq('Buzz'))
 
     it('will return Buzz if the input is 20', () =>
-      expect(app.getSequence()[20 - 1]).to.be.eq('Buzz'))
+      expect(getSequencePosition(20)).to.be.eq('Buzz'))
 
     it('will return FizzBuzz if the input is 15', () =>
-      expect(app.getSequence()[15 - 1]).to.be.eq('FizzBuzz'))
+      expect(getSequencePosition(15)).to.be.eq('FizzBuzz'))
 
     it('will return FizzBuzz if the input is 30', () =>
-      expect(app.getSequence()[30 - 1]).to.be.eq('FizzBuzz'))
+      expect(getSequencePosition(30)).to.be.eq('FizzBuzz'))
   })
 })
+
+function getSequencePosition (number) {
+  return app.getSequence()[number - 1]
+}
