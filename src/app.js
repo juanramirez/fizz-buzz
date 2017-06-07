2f99582a634f23d7d04afa964a2bde39edfadb71
Juan Ramírez Ruiz <juan.ramirez.ruiz@gmail.com>
2017-06-07 12:22:19 +0200

    Refactor to return make the code simpler.

--- src/app.js
+++ src/app.js
@@ -1,24 +1,13 @@
 let _ = require('lodash')
 
 let translate = input => {
-  if (input === 53)
-    return 'FizzBuzz'
-  if (input === 35)
-    return 'FizzBuzz'
-  if (input % 15 === 0)
-    return 'FizzBuzz'
-  if (input % 5 === 0)
-    return 'Buzz'
-  if (input % 3 === 0) {
-    if (_.includes(input.toString(), '5'))
-      return 'FizzBuzz'
-    return 'Fizz'
-  }
-  if (_.includes(input.toString(), '5'))
-    return 'Buzz'
-  if (_.includes(input.toString(), '3'))
-    return 'Fizz'
-  return input.toString()
+  let fizz = input % 3 === 0 || _.includes(input.toString(), '3')
+  let buzz = input % 5 === 0 || _.includes(input.toString(), '5')
+
+  if (!fizz && !buzz)
+    return input.toString()
+  else
+    return (fizz ? 'Fizz' : '') + (buzz ? 'Buzz' : '')
 }
 
 module.exports = {
